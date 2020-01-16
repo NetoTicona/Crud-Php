7cb4a339724c1cee7b41c2ae503a92a7ebd9930c
koko <jticonaquisp@unsa.edu.pe>
2020-01-16 11:49:59 -0500

    agregando el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,60 @@
+<footer class="footer-distributed">
+ 
+			<div class="footer-left">
+          <img src="img/logo.png">
+				<h3>nosotros<span>AustralTel</span></h3>
+ 
+				<p class="footer-links">
+					<a href="#">Inicio</a>
+					|
+					<a href="#">Blog</a>
+					|
+					<a href="#">Acerca de</a>
+					|
+					<a href="#">Contactanos</a>
+				</p>
+ 
+				<p class="footer-company-name">© 2020 Austral telecomunicaiones creando soluciones. sac.</p>
+			</div>
+ 
+			<div class="footer-center">
+				<div>
+					<i class="fa fa-map-marker"></i>
+					  <p><span>Coop. Villa El Sol,
+						 Bldg. No. I - 10, Sector - 1</span>
+					Pampas de polanco Alto Selva Alegre </p>
+				</div>
+ 
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+51 957787056</p>
+				</div>
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@eduonix.com">netoticonaquispe@gmail.com</a></p>
+				</div>
+			</div>
+			<div class="footer-right">
+				<p class="footer-company-about">
+					<span>Acerca de la compañia</span>
+					Ofrecemos diseño y desarrollo web para todos.</p>
+				<div class="footer-icons">
+					<a href="https://www.facebook.com"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-instagram"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube"></i></a>
+				</div>
+			</div>
+		</footer>
+
+
+
+
+
+
+
+
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>    
